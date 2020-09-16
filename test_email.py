f2bbce5c9f725e4830f5cdced8f75eed4cee104f
MasakoKh <ly.sitthykun@gmail.com>
2020-09-16 10:37:54 +0700

    remove test file

--- test_email.py
+++
@@ -1,8 +0,0 @@
-import re
-example = "example@gmial.com@"
-
-if re.search("@gm(ia|a|i)l.com$", example):
-    print("Maybe you meant @gmail.com?")
-
-if re.search("^[a-z]([w-]*[a-z]|[w-]*[a-z]{2,}|[a-z])*@[a-z]([w-]*[a-z]|[w-]*[a-z]{2,}|[a-z]){4,}?.[a-z]{2,}$", example):
-    print("Full")
